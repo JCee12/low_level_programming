e86c317526503eacb351f9f7662a48f97546b219
Jackline Muraya <jacklinewanja.jw@gmail.com>
2022-09-28 13:33:38 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,19 +2,15 @@
 
 /**
  * factorial - entry point
- * @n: input 
+ * @n: input
  * Return: 0
  */
 
 int factorial(int n)
 {
-	if (n == 0)
-	{
-		return (1);
-	}
-	else if (n < 0)
-	{
+	if (n < 0)
 		return (-1);
-	}
+	if (n == 0)
+		return (1);
 	return (n * factorial(n - 1));
 }

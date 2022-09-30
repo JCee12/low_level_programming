76c19f4ef5478eb6aa3768a169393242ad74cbff
Jackline Muraya <jacklinewanja.jw@gmail.com>
2022-09-30 16:32:19 -0700

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * Return: Always (0)
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
+

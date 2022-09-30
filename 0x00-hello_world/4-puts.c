ac78ea03bcc42b76b9f64f1e667113e5bca48516
Jackline Muraya <jacklinewanja.jw@gmail.com>
2022-09-30 14:10:11 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - Write '"Programming is like building a multilingual puzzle'
+ *
+ *   * Return: Always 0 (success)
+ *
+ *    */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
+
+

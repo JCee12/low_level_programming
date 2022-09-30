cfea558b8b0f5efe70574b451f4f2ed7d4feac9d
Jackline Muraya <jacklinewanja.jw@gmail.com>
2022-09-30 16:48:03 -0700

    7-print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *
+ *  * print_diagonal - draws a diagonal line
+ *
+ *   * @n: input value
+ *
+ *    * Return: Always (0)
+ *
+ *     */
+
+void print_diagonal(int n)
+{
+	int count = 0, size;
+
+	if (n > 0)
+	{
+		while (count < n)
+		{
+			for (size = 0; size < count; size++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			count++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

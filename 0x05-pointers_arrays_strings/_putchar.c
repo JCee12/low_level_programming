62433a0a2c6dfecc8e3dfbd0d834e54fd886b87a
Jackline Muraya <jacklinewanja.jw@gmail.com>
2022-09-30 17:11:18 -0700

    readme

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+
+
+/**
+ *
+ *  * _putchar - writes character c to stdout
+ *
+ *   * @c: the character to print
+ *
+ *    *
+ *
+ *     * Return: 1 (success) and -1 (error)
+ *
+ *      */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

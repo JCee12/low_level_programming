486411534ef8db93ad2792a7dac34d2cc0cf5e8e
Jackline Muraya <jacklinewanja.jw@gmail.com>
2022-09-30 17:16:23 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+/**
+ *
+ *  * _puts - print string
+ *
+ *   * @str: input string
+ *
+ *    * Return: no return
+ *
+ *     */
+
+
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}

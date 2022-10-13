bb9954d78f9bc6d0cd1eaf75b015c47d75ea6fa0
Jackline Muraya <jacklinewanja.jw@gmail.com>
2022-10-13 12:51:57 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,20 +2,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name: function that prints a name
- * @name: name of the person
- * @f: pointer
- * Return: 0
+ * print_name - prints a name
+ * @name: pointer to name
+ * @f: function pointer
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-	{
-		return;
-	}
-	else
-	{
+	if (name && f)
 		f(name);
-	}
 }

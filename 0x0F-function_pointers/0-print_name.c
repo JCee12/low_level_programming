2c24a00a1d18b3cb9dde20aa0a1e57e76e9a3f4c
Jackline Muraya <jacklinewanja.jw@gmail.com>
2022-10-13 12:44:13 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name: function that prints a name
+ * @name: name of the person
+ * @f: pointer
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+	{
+		f(name);
+	}
+}

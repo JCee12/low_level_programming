7acf36ec89044e57e2b731d00dab9d30de99e86a
Jackline Muraya <jacklinewanja.jw@gmail.com>
2022-10-13 16:02:41 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,10 +2,9 @@
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name 
- * @name: input name
- * @f: function pointer
- * Return: Nothing
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer
  */
 
 void print_name(char *name, void (*f)(char *))
